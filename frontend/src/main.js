98a4589b3c4a51d690b77a967822c3d2823fe32d
Sujit Patel <97437584+sujitpatel22@users.noreply.github.com>
2024-10-20 22:04:50 +0530

    Delete frontend directory

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router'; // Import your router
-
-new Vue({
-  render: (h) => h(App),
-  router, // Register the router
-}).$mount('#app');
