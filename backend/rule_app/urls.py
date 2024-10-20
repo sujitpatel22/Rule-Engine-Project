7909950b8617d553730fbbabacef1892c31edcd7
Sujit Patel <97437584+sujitpatel22@users.noreply.github.com>
2024-10-20 22:04:35 +0530

    Delete backend directory

--- backend/rule_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create_rule/', views.create_rule, name='create_rule'),
-    path('combine_rules/', views.combine_rules, name='combine_rules'),
-    path('evaluate_rule/', views.evaluate_rule, name='evaluate_rule'),
-]
